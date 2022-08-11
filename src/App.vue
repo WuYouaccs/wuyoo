03447ca04b2fe9a8df5f89cab9b77e73f421b858
WuYouaccs <89030087+WuYouaccs@users.noreply.github.com>
2022-08-12 00:29:26 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,13 @@ div(ref="affix")
         .btn
           a(href="https://accsou.com/" target="_blank" )  24小时自助购买地址（需要科学，打不开请用下面地址或联系客服）
         .btn
-          a(hhef="https://accsou.xyz/" target="_blank")   24小时自助购买地址(关注频道获取优惠码,价格更优惠）
+          a(href="https://accsou.xyz/" target="_blank")   24小时自助购买地址(关注频道获取优惠码,价格更优惠）
         .btn
-          a(hhef="https://t.me/yamaiddi" target="_blank")   请认准唯一客服@yamaiddi(可联系客服直接购买)        
+          a(href="https://t.me/yamaiddi" target="_blank")   请认准唯一客服@yamaiddi(可联系客服直接购买)        
         .btn
-          a(hhef="https://t.me/accsoutop" target="_blank")   请关注telegram频道（补货通知，优惠码发放）        
+          a(href="https://t.me/accsoutop" target="_blank")   请关注telegram频道（补货通知，优惠码发放）        
         .btn
-          a(hhef="http://wpa.qq.com/msgrd?v=3&uin=23676616&site=qq&menu=yes" target="_blank")   唯一QQ客服（QQ：23676616）     
+          a(href="http://wpa.qq.com/msgrd?v=3&uin=23676616&site=qq&menu=yes" target="_blank")   唯一QQ客服（QQ：23676616）     
                 
     n-gi(span="1 s:2 m:2 l:5 xl:6 2xl:6")
 
